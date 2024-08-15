bb16a09440d7dbab2b33af81ca5c6a9a8a91b242
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-08-15 16:38:23 +0530

    Time: 165 ms (57.73%), Space: 10.3 MB (92.52%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    
     int dp[305][305];
-    int func(int i , int j, vector <int> &nums){
+
+    int func(int i, int j, vector <int> &nums){
         if(i > j) return 0;
-        if(dp[i+1][j+1] != -1) return dp[i+1][j+1];
-        int ans = 0;
-        for(int k = i+1 ; k < j ; k++){
-            int l = (i == -1) ? 1 : nums[i];
-            int r = (j == nums.size()) ? 1 : nums[j];
-            int t = l*r*nums[k];
-            ans = max(ans, func(i, k, nums) + func(k, j, nums) + t);
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = INT_MIN;
+        for(int k = i ; k <= j ; k++){
+            //kth index is the last balloon to get popped within the suubarray i->j
+            int lastCoins = nums[i-1]*nums[k]*nums[j+1];
+            int left = func(i,k-1,nums);
+            int right = func(k+1,j,nums);
+            ans = max(ans, lastCoins + left + right);
         }
-        return dp[i+1][j+1] = ans;
-    }
-    int maxCoins(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return func(-1, nums.size(), nums);
+        return dp[i][j] = ans;
     }
 
+    int maxCoins(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        return func(1,n,nums);
+    }
 };
