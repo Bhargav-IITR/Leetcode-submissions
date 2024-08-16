8d407bb5125b9588dfce5f8714828f758b1bd042
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-08-17 02:19:52 +0530

    Time: 13 ms (63.94%), Space: 14.4 MB (33.76%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,31 +1,77 @@
 #define ll long long
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
+    vector<int> NSE(vector<int>&nums){
+        stack<int>st;
         int n = nums.size();
-        vector<vector<pair<int,int>>> minmax(n,vector<pair<int,int>>(n));
-        for(int i = 0 ; i < n ; i++){
-            minmax[i][i] = {nums[i], nums[i]};
-        }
-        for(int i = 0 ; i < n-1 ; i++){
-            minmax[i][i+1] = {min(nums[i], nums[i+1]), max(nums[i], nums[i+1])};
-        }
-        for(int len = 3 ; len <= n ; len++){
-            for(int i = 0 ; i < n ; i++){
-                int j = i+len-1;
-                if(j < n){
-                    int mx = max(nums[i], minmax[i+1][j].second);
-                    int mn = min(nums[i], minmax[i+1][j].first);
-                    minmax[i][j] = {mn, mx};
-                }
+        vector<int> nse(n,0);
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
             }
+            if(st.empty()){
+                nse[i] = n;
+            }
+            else{
+                nse[i] = st.top();
+            }
+            st.push(i);
         }
+        return nse;
+    }
+    vector<int> PSE(vector<int>&nums){
+        stack<int> st;
+        int n = nums.size();
+        vector<int> pse(n,0);
+        for(int i=0 ;i<n;i++){
+            while(!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            if(st.empty()) pse[i] =  -1;
+            else pse[i] = st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+    vector<int> NGE(vector<int>&nums){
+        stack<int> st;
+        int n = nums.size();
+        vector<int> nge(n,0);
+        for(int i= n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            if(st.empty()){
+                nge[i] = n;
+            }
+            else{
+                nge[i] = st.top();
+            }
+            st.push(i);
+        }
+        return nge;
+    }
+    vector<int> PGE(vector<int>&nums){
+        stack<int>st;
+        int n = nums.size();
+        vector<int> pge(n,0);
+        for(int i = 0;i<n;i++){
+            while(!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            if(st.empty()) pge[i] = -1;
+            else pge[i] = st.top();
+            st.push(i);
+        }
+        return pge;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        vector <int> nge = NGE(nums);
+        vector <int> pge = PGE(nums);
+        vector <int> nse = NSE(nums);
+        vector <int> pse = PSE(nums);
         ll ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i+1 ; j < n ; j++){
-                ans += 1l*(minmax[i][j].second-minmax[i][j].first);
-            }
+        for(int i = 0 ; i < nums.size() ; i++){
+            //take it as max:
+            ll lmax = i-pge[i], rmax = nge[i]-i;
+            //take it as min:
+            ll lmin = i-pse[i], rmin = nse[i]-i;
+            ans += (lmax*rmax - lmin*rmin)*nums[i];            
         }
-        return ans;
+        return ans;        
     }
 };
