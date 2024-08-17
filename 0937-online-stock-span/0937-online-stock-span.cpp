a4d04e392c8dc8b27b1acc6391c5aaba1c803a4e
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-08-17 15:49:08 +0530

    Time: 178 ms (25.94%), Space: 90.4 MB (25.16%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,20 +1,36 @@
 class StockSpanner {
 public:
-    stack <int> st;
-    map <int,int> mp;
+    // stack <int> st;
+    // map <int,int> mp;
+    // StockSpanner() {
+    // }
+    
+    // int next(int price) {
+    //     int ans = 1;
+    //     while(!st.empty() && st.top() <= price){
+    //         int t = st.top();
+    //         st.pop();
+    //         ans += mp[t];
+    //     }
+    //     st.push(price);
+    //     return mp[price] = ans;
+    // }
+
+    stack <pair<int,int>> st;
     StockSpanner() {
     }
     
     int next(int price) {
         int ans = 1;
-        while(!st.empty() && st.top() <= price){
-            int t = st.top();
+        while(!st.empty() && st.top().first <= price){
+            auto t = st.top();
             st.pop();
-            ans += mp[t];
+            ans += t.second;
         }
-        st.push(price);
-        return mp[price] = ans;
+        st.push({price, ans});
+        return ans;
     }
+
 };
 
 /**
