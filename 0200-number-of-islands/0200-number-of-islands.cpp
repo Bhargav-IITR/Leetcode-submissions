a33c2a505b84c8ddcb1efd39b44a217cbd50be04
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-08-22 15:33:54 +0530

    Time: 68 ms (9.94%), Space: 16.2 MB (64.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,97 @@
+
+class DisjointSet{
+    vector <int> rank, parent, size;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        for(int i = 0 ; i <= n ; i++) parent[i] = i; 
+    }
+
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v){
+        //we have node u and node v which have to be
+        //combined
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    //time complexity = O(4*alpha) == O(constant); 
+};
+
 class Solution {
 public:
-    void dfs(int i , int j , vector<vector<int>> &vis, vector<vector<char>> &grid){
-        int m = grid.size(), n = grid[0].size();
-        if(i < 0 || j < 0 || i >= m || j >= n) return;
-        if(grid[i][j] == '0') return;
-        if(vis[i][j] == 1) return;
-        vis[i][j] = 1;   
-        dfs(i-1,j,vis,grid);
-        dfs(i+1,j,vis,grid);
-        dfs(i,j-1,vis,grid);
-        dfs(i,j+1,vis,grid);
+    bool isVal(int i, int j, int n, int m){
+        return (i < n && j < m && i >= 0 && j >= 0);
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int> (n,0));
-        int ans = 0;
-        for(int i =0;i<m;i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == '1' && vis[i][j] == 0){
-                    ans++;
-                    dfs(i,j,vis,grid);
+        int nxt = 1;
+        int n = grid.size(), m = grid[0].size();
+        vector <vector <int>> list(n, vector <int> (m, 0));
+        for(int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == '1') list[i][j] = nxt;
+                else list[i][j] = 0;
+                nxt++;
+            }
+        }
+
+        vector <pair <int,int>> moves = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+
+        DisjointSet ds(n*m);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(list[i][j] != 0){
+                    for(auto e : moves){
+                        int ni = i + e.first;
+                        int nj = j + e.second;
+                        if(isVal(ni, nj, n, m) && list[ni][nj] != 0){
+                            ds.unionByRank(list[i][j], list[ni][nj]);
+                        }
+                    }
                 }
             }
         }
-        return ans;
+
+        set <int> st;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(list[i][j] != 0){
+                    int par = ds.findParent(list[i][j]);
+                    st.insert(par);
+                }
+            }
+        }
+        return st.size();
     }
 };
