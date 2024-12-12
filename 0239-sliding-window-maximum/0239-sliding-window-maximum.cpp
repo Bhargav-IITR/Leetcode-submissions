938acf7592b4dc1ba44e398ecba8e6350775a00b
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-12-12 20:52:09 +0530

    Time: 21 ms (88.05%), Space: 139.2 MB (38.29%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque <int> dq; // stores the indices in a decreasing manner of their value from left to right
         // the left of dq for any valid window will give the maximum of that window
-        vector <int> ans;
+        vector<int> ans;
         for(int i = 0 ; i < nums.size() ; i++){
-            while(!dq.empty() && dq.front() < i-k+1){ // ensuring the elements fall in the window k
-                dq.pop_front();
-            }
+            while(!dq.empty() && dq.front() < i-k+1) dq.pop_front();
             while(!dq.empty() && nums[dq.back()] <= nums[i]){
                 dq.pop_back();
             }
