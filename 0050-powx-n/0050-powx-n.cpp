15633e22e9dcd0a93d6881a2056f6dd968e70d16
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-06-02 14:52:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
+    double func(double x, long long int n){
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        if(n > 0){
+            double res = myPow(x, n/2);
+            if(n&1) return x*res*res;
+            return res*res;
+        }else{
+            n = abs(n);
+            double res = myPow(x, n/2);
+            if(n&1) return 1.0/(x*res*res);
+            return 1.0/(res*res);
+        }
+    }
     double myPow(double x, int n) {
-        if(n == INT_MAX) return (x == 1)?1:(x == -1)?-1:0;
-        if(n == INT_MIN) return (x == 1|| x==-1)?1:0;
-        if(n == 0) return 1.0;
-        if(n > 0) return x*myPow(x, n-1);
-        return 1.0/(x*myPow(x, abs(n)-1));
+        return func(x, (long long int)n);
     }
 };
