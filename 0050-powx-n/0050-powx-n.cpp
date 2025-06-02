589380dd31f06f8a92c7bf23a1ab9a145348e41a
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-06-02 14:30:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == INT_MAX) return (x == 1)?1:(x == -1)?-1:0;
+        if(n == INT_MIN) return (x == 1|| x==-1)?1:0;
+        if(n == 0) return 1.0;
+        if(n > 0) return x*myPow(x, n-1);
+        return 1.0/(x*myPow(x, abs(n)-1));
+    }
+};
