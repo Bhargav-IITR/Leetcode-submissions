fc9cae66324bf48acde385bc6cb87f3adb1556b7
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-06-04 17:04:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (5.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> setupLPS(string &s) {
-    int n = s.length();
-    vector<int> lps(n); // Initialize LPS array
-    lps[0] = 0; // The first LPS value is always 0
-    int prevlps = 0; // Length of the previous longest prefix suffix
-    int i = 1; // Start from the second character
-
-    while (i < n) {
-        if (s[i] == s[prevlps]) {
-            lps[i] = prevlps + 1;
-            prevlps++;
-            i++;
-        } else {
-            if (prevlps == 0) {
-                lps[i] = 0;
-                i++;
-            } else {
-                prevlps = lps[prevlps - 1];
-            }
+        int n = s.length();
+        vector <int> pi(n, 0);
+        for(int i = 1 ; i < n ; i++){
+            int j = pi[i-1];
+            while(j > 0 && s[i] != s[j]) j = pi[j-1];
+            if(s[j] == s[i]) j++;
+            pi[i] = j;
         }
+        return pi;
     }
-    return lps; // Correctly return the LPS array
-}
 
     int strStr(string haystack, string needle) {
         vector <int> lps = setupLPS(needle);
@@ -30,12 +18,11 @@ public:
         while(i < haystack.length()){
             if(haystack[i] == needle[j]){
                 i++,j++;
+            }else{
+                if(j != 0) j = lps[j-1];
+                else i++;
             }
-            else{
-                if(j == 0) i++;
-                else j = lps[j-1];
-            }
-            if(j == needle.length()) return i-needle.length();
+            if(j == needle.length()) return i-j;
         }
         return -1;
     }
