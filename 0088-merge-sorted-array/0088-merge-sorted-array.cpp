59468e092ed5e2a44bc44a266bcf457863e8e698
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-07-10 11:45:15 +0530

    Time: 0 ms (100%), Space: 12.1 MB (95.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     //GAP METHOD:
+    //     int gap = (m+n+1)/2;
+    //     while(gap > 0){
+    //         int i = 0, j = i+gap;            
+    //         while(j < m+n){
+    //             if(i < m && j >= m){ // n1 & n2
+    //                 if(nums1[i] > nums2[j-m]){
+    //                     swap(nums1[i], nums2[j-m]);
+    //                 }
+    //             }else if(j < m){ // n1 & n1
+    //                 if(nums1[i] > nums1[j]){
+    //                     swap(nums1[i], nums1[j]);
+    //                 }
+    //             }else{ // n2 & n2
+    //                 if(nums2[i-m] > nums2[j-m]){
+    //                     swap(nums2[i-m], nums2[j-m]);
+    //                 }
+    //             }
+    //             i++, j++;
+    //         }            
+    //         if(gap == 1) break;
+    //         gap = (gap+1)/2;
+    //     }
+    //     for(int i = m ; i < n+m ; i++) nums1[i] = nums2[i-m];
+    // }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //GAP METHOD:
-        int gap = (m+n+1)/2;
-        while(gap > 0){
-            int i = 0, j = i+gap;            
-            while(j < m+n){
-                if(i < m && j >= m){ // n1 & n2
-                    if(nums1[i] > nums2[j-m]){
-                        swap(nums1[i], nums2[j-m]);
-                    }
-                }else if(j < m){ // n1 & n1
-                    if(nums1[i] > nums1[j]){
-                        swap(nums1[i], nums1[j]);
-                    }
-                }else{ // n2 & n2
-                    if(nums2[i-m] > nums2[j-m]){
-                        swap(nums2[i-m], nums2[j-m]);
-                    }
-                }
-                i++, j++;
-            }            
-            if(gap == 1) break;
-            gap = (gap+1)/2;
+        int last = m+n-1;
+        int i = m-1, j = n-1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[last] = nums1[i];
+                i--;
+            }else{
+                nums1[last] = nums2[j];
+                j--;
+            }
+            last--;
         }
-        for(int i = m ; i < n+m ; i++) nums1[i] = nums2[i-m];
+        while(j >= 0){
+            nums1[last] = nums2[j];
+            last--, j--;
+        }
     }
 };
