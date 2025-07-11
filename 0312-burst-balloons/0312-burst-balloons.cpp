7bd4c05f081011ab503a660f5d4b9183d6f0dd3d
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-07-11 10:54:55 +0530

    Time: 161 ms (52.09%), Space: 10.9 MB (98.13%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    
-    int dp[305][305];
 
+    int dp[304][304];
     int func(int i, int j, vector <int> &nums){
         if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int ans = INT_MIN;
         for(int k = i ; k <= j ; k++){
-            //kth index is the last balloon to get popped within the suubarray i->j
-            int lastCoins = nums[i-1]*nums[k]*nums[j+1];
-            int left = func(i,k-1,nums);
-            int right = func(k+1,j,nums);
-            ans = max(ans, lastCoins + left + right);
+            int t = nums[i-1]*nums[k]*nums[j+1] + func(i, k-1, nums) + func(k+1, j, nums);
+            ans = max(ans, t);
         }
         return dp[i][j] = ans;
     }
