e96737f5e450ddea7481d26565e5776b8c8987a0
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-12-25 20:56:45 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // // moore's algorithm
-        // int can = -1, cnt = 0;
-        // for(int i = 0 ; i < nums.size(); i++){
-        //     if(cnt == 0){
-        //         can = nums[i];
-        //         cnt++;
-        //     }
-        //     else{
-        //         if(nums[i] == can) cnt++;
-        //         else cnt--;
-        //     }
-        // }
-        // cnt = 0;
-        // for(int i = 0 ; i < nums.size(); i++) cnt += (nums[i] == can);
-        // if(cnt > nums.size()/2) return can;
-        // return -1;
-        int num = nums[0];
-        int cnt = 1;
-        for(int i = 1 ; i < nums.size() ; i++){
-            if(nums[i] != num){
-                cnt--;
-                if(cnt == 0){
-                    cnt = 1;
-                    num = nums[i];
-                }
-            }else cnt++;
+        int cnt = 0, el;
+        if(nums.size() == 1) return nums[0];
+        for(auto it : nums){
+            if(cnt == 0) el = it;
+            if(it == el) cnt++;
+            else cnt--;
         }
-        cnt = 0;
-        for(int i = 0 ; i < nums.size() ; i++) cnt += (nums[i] == num);
-        if(cnt > nums.size()/2) return num;
-        return -1;
+        return el;
     }
 };
