a0df41bed42cea63795748c771f4fdf41e0756aa
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-01-18 19:44:42 +0530

    Time: 176 ms (13.89%), Space: 11 MB (77.49%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int beautySum(string s) {
-
-        vector <vector <int>> pf(26, vector <int> (s.length(), 0));
-        for(int i = 0; i < s.length() ;i++){
+        int n = s.length();
+        vector <vector<int>> pf(26, vector <int> (n, 0));
+        for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < 26 ; j++){
-                if(i == 0) pf[j][i] = (j == s[i]-'a');
-                else pf[j][i] += pf[j][i-1] + (j == s[i]-'a');
+                pf[j][i] = (s[i]-'a' == j) +( (i == 0)?0 : pf[j][i-1]);
             }
         }
-
         int ans = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            for(int j = i+1 ; j < s.length() ; j++){
-                int mx = INT_MIN, mn = INT_MAX;
+        // generate all substrings and calculate for each
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                int mx = INT_MIN, mn = INT_MAX; // max and min frequencies
                 for(int k = 0 ; k < 26 ; k++){
-                    int l = (i == 0) ? 0:pf[k][i-1];
-                    int r = pf[k][j];
-                    int f = r-l;
-                    mx = max(mx, f);
-                    mn = (f != 0)?min(mn, f):mn;
+                    int freqK = pf[k][j] - (i==0?0:pf[k][i-1]);
+                    // if(freqK != 0){
+                    //     mx = max(mx, freqK);
+                    //     mn = min(mn, freqK);
+                    // }
+                    mx = max(mx, freqK);
+                    mn = (freqK!=0)?min(freqK, mn):mn;
                 }
-                ans += mx-mn;
+                ans += (mx-mn);
             }
         }
         return ans;
