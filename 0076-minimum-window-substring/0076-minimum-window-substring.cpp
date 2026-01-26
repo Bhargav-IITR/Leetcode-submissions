9d6a9ccd37c256183a5f91a288e3b641c0014891
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-01-26 20:36:08 +0530

    Time: 16 ms (30.14%), Space: 11.3 MB (93.79%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m = s.length(), n = t.length();
-        if(m < n) return "";
         map <char, int> mp;
-        for(int i = 0 ; i < n ; i++) mp[t[i]]++;
-        int l = 0, r = 0, cnt = 0;
-        int len = INT_MAX, start = -1;
-        while(r < m){
+        int n = s.length(), m = t.length();
+        if(n < m) return "";
+        for(int i = 0 ; i < m ; i++) mp[t[i]]++;
+        int l = 0, r = 0, cnt = 0, len = 1e9, si = -1;
+
+        while(r < n){
             if(mp[s[r]] > 0) cnt++;
             mp[s[r]]--;
-            while(cnt == n){
-                if(r-l+1 < len){
-                    len = r-l+1;
-                    start = l;
-                }
+            while(cnt == m){
                 mp[s[l]]++;
                 if(mp[s[l]] > 0) cnt--;
+                if(r-l+1 < len){
+                    len = r-l+1;
+                    si = l;
+                }
                 l++;
             }
             r++;
-        }
-        if(start == -1) return "";
-        return s.substr(start, len);        
+        }   
+        if(len == 1e9) return "";
+        return s.substr(si, len);
     }
 };
