8fae10a137ecc61c186be35239f6d74a73467fa6
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-01-29 10:33:33 +0530

    Time: 0 ms (100%), Space: 27 MB (39.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // tc- o(n) , sc - o(n)
-        // stack <int> st;
-        // int n = height.size();
-        // int ans = 0;
-        // for(int i = 0 ; i < n ; i++){
-        //     while(!st.empty() && height[st.top()] <= height[i]){
-        //         int currHeight = height[st.top()];
-        //         st.pop();
-        //         if(st.empty()) break;
-        //         int leftHeight = height[st.top()];
-        //         int rightHeight = height[i];
-        //         int width = i-st.top()-1;
-        //         ans += (min(leftHeight,rightHeight)-currHeight)*width;
-        //     }
-        //     st.push(i);
-        // }
-        // return ans;
-
-        // tc - o(n) , sc - o(1):
+        int ans = 0;
         int n = height.size();
-        int l = 0, r = n-1, lmax = 0, rmax = 0, total = 0;
-
-        while(l < r){
-            if(height[l] <= height[r]){
-                if(lmax > height[l]){
-                    total += (lmax-height[l]);
-                }
-                else lmax = height[l];
-                l++;
-            }else{
-                if(rmax > height[r]){
-                    total += (rmax-height[r]);
-                }
-                else rmax = height[r];
-
-                r--;
+        stack <int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && height[st.top()] <= height[i]){
+                int base = height[st.top()];
+                st.pop();
+                int left = (!st.empty())?height[st.top()]:-1;
+                int right = height[i];
+                int width = i-1-((st.empty())?-1:st.top());
+                if(left != -1) ans += (min(left,right)-base)*width;
             }
+            st.push(i);
         }
-        return total;
+        return ans;
     }
 };
