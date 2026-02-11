3b1cba733a160de8983430e8fe718e12336a4626
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-02-11 23:42:09 +0530

    Time: 419 ms (26.09%), Space: 41.7 MB (33.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int dp[2501][2502];
+     int dp[2501][2502];
     int func(vector <int> &nums, int i, int prev){
         int n = nums.size();
-        if(i >= n) return 0;
+        if(i < 0) return 0;
         if(dp[i][prev+1] != -1) return dp[i][prev+1];
-        int notTake = func(nums, i+1, prev);
+        int notTake = func(nums, i-1, prev);
         int take = 0;
-        if(prev == -1 || (nums[prev] < nums[i])) take = 1 + func(nums, i+1, i);
+        if(prev == -1 || (nums[prev] > nums[i])) take = 1 + func(nums, i-1, i);
         return dp[i][prev+1] = max(take, notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));
-        return func(nums, 0, -1);
+        return func(nums, nums.size()-1, -1);
+        // int n = nums.size();
+        // vector <vector <int>> dp(n+1, vector <int> (n+1, -1));
+        // for(int j = 0 ; j <= n ; j++) dp[n][j] = 0;
+        // for()
     }
 };
