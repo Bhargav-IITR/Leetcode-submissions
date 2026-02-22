32f73c04176276b7304eedef706a5d2d5c817b8c
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-02-22 13:04:22 +0530

    Time: 5 ms (44.4%), Space: 16.4 MB (10.03%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void makeParent(TreeNode* root, map <TreeNode*, TreeNode*> &parent){
+        if(!root) return;
+        if(root->left){
+            parent[root->left] = root;
+            makeParent(root->left, parent);
+        }
+        if(root->right){
+            parent[root->right] = root;
+            makeParent(root->right, parent);
+        }
+    }
+    void func(TreeNode* node, map <TreeNode*, TreeNode*> &parent, int k, vector <int> &ans, map <TreeNode*, int> &vis){
+        if(!node) return;
+        if(vis[node] == 1) return;
+        if(k == 0){
+            ans.push_back(node->val);
+            return;
+        }
+        vis[node] = 1;
+        func(node->left, parent, k-1, ans, vis);
+        func(node->right, parent, k-1, ans, vis);
+        if(parent[node] != node) func(parent[node], parent, k-1, ans, vis);
+        vis[node] = 0;
+    }   
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        if(!root) return {};
+        map <TreeNode*, TreeNode*> parent;
+        map <TreeNode*, int> vis;
+        makeParent(root, parent);
+        parent[root] = root;
+        vector <int> ans;
+        func(target, parent, k, ans, vis);
+        return ans;
+    }
+}; 
