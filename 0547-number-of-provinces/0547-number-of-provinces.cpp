e13ef7b211267db754466324a2adb8adf2473675
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-02-28 11:15:48 +0530

    Time: 0 ms (100%), Space: 19.5 MB (73.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,22 +1,26 @@
 class Solution {
-public:
-void dfs(int node, vector <vector <int>> &adj, vector<int> &vis, int n){
-        if(vis[node] == 1) return;
-        vis[node] = 1;
-        for(int nxt = 0 ; nxt < n ; nxt++){
-            if(nxt != node && adj[node][nxt] == 1){
-                dfs(nxt,adj,vis,n);
+public: 
+    void dfs(int v, vector<int> &vis, vector <vector <int>> &isConnected){
+        int n = vis.size();
+        for(int adj = 0 ; adj < n ; adj++){
+            if(isConnected[v][adj] &&(v != adj)){
+                if(vis[adj] == 0){
+                    vis[adj] = 1;
+                    dfs(adj, vis, isConnected);
+                }
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& adj) {
-        int n = adj.size();
-        vector <int> vis(n,0);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        // givem is adjacency matrix 
+        int n = isConnected.size();
+        vector <int> vis(n, 0);
         int ans = 0;
-        for(int node = 0 ; node < n ; node++){
-            if(vis[node] == 0){
+        for(int i = 0 ; i < n ; i++){
+            if(vis[i] == 0){
                 ans++;
-                dfs(node, adj, vis, n);
+                vis[i] = 1;
+                dfs(i, vis, isConnected);
             }
         }
         return ans;
