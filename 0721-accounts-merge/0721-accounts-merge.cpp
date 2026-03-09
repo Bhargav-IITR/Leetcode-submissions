d95438fd0db5ca9ff4815fb0554d2665a842a19b
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-03-10 00:33:00 +0530

    Time: 23 ms (92.25%), Space: 40.5 MB (46.01%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,60 +1,86 @@
-class disjointSet{
-    vector <int> rank, parent;
-  public:
-    disjointSet(int n){
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        for(int i = 0 ; i <= n ; i++) parent[i] = i;
+class DSU{
+public:
+    vector<int> parent, rank;
+
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
     }
-    
-    int findParent(int u){
-        if(parent[u] == u) return u;
-        return parent[u] = findParent(parent[u]); // path compression
+
+    int findParent(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=findParent(parent[x]);
     }
-    
-    void unionByRank(int u, int v){
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-        
-        int ru = rank[ulp_u], rv = rank[ulp_v];
-        if(ru < rv){
-            parent[ulp_u] = ulp_v;
-        }else if(rv < ru){
-            parent[ulp_v] = ulp_u;
-        }else{
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
+
+    void unite(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+
+        if(pu==pv) return;
+
+        if(rank[pu]<rank[pv])
+            parent[pu]=pv;
+        else if(rank[pv]<rank[pu])
+            parent[pv]=pu;
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
         }
     }
 };
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
-        disjointSet dsu(n);
-        unordered_map <string, int> stringNode;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 1 ; j < accounts[i].size() ; j++){
-                string s = accounts[i][j];
-                if(stringNode.find(s) == stringNode.end()) stringNode[s] = i;
-                else{
-                    dsu.unionByRank(i, stringNode[s]);
-                }
+
+        int n=accounts.size();
+        DSU dsu(n);
+
+        unordered_map<string,int> mailToAcc;
+
+        // Step 1: union accounts sharing email
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+
+                string mail=accounts[i][j];
+
+                if(mailToAcc.count(mail)==0)
+                    mailToAcc[mail]=i;
+                else
+                    dsu.unite(i, mailToAcc[mail]);
             }
         }
-        vector <vector<string>> temp(n);
-        for(auto it : stringNode){
-            int ulNode = dsu.findParent(it.second);
-            temp[ulNode].push_back(it.first);
+
+        // Step 2: collect emails by parent
+        unordered_map<int,set<string>> mp;
+
+        for(auto &it: mailToAcc){
+            string mail=it.first;
+            int acc=it.second;
+
+            int parent=dsu.findParent(acc);
+
+            mp[parent].insert(mail);
         }
-        vector <vector <string>> ans;
-        for(int i = 0 ; i < temp.size() ; i++){
-            if(temp[i].size() == 0) continue;
-            vector <string> t = temp[i];
-            sort(t.begin(), t.end());
-            t.insert(t.begin(), accounts[i][0]);
-            ans.push_back(t);
+
+        // Step 3: build answer
+        vector<vector<string>> ans;
+
+        for(auto &it: mp){
+
+            int accIndex=it.first;
+
+            vector<string> temp;
+
+            temp.push_back(accounts[accIndex][0]);
+
+            for(auto &mail: it.second)
+                temp.push_back(mail);
+
+            ans.push_back(temp);
         }
+
         return ans;
     }
 };
