82950e3fc0ddc24609157acd491f5aa64b24f5bb
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-03-11 23:39:49 +0530

    Time: 15 ms (30.95%), Space: 16 MB (15.94%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,41 +1,61 @@
+class DSU{
+    vector <int> parent, rank;
+    
+    public:
+        DSU(int n){
+            parent.resize(n+1);
+            rank.resize(n+1, 0);
+            for(int i = 0 ;i < n+1; i++) parent[i] = i;;            
+        }
+
+        int findParent(int u){
+            if(parent[u] == u) return u;
+            int p = findParent(parent[u]);
+            return parent[u] = p;
+        }
+
+        void unionByRank(int u, int v){
+            int upu = findParent(u);
+            int upv = findParent(v);
+            if(upu == upv) return;
+            if(rank[upu] > rank[upv]){
+                parent[upv] = upu;
+            }else if(rank[upu] < rank[upv]){
+                parent[upu] = upv;
+            }else{
+                parent[upu] = upv;
+                rank[upv]++;
+            }
+        }
+};
+
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        /*
-        bruteforce: t = 0, while(t++){
-            check if top left and bottom right are same comp
-            if not same-
-            traverse(grid){
-                node = (i,j)
-                go to adj nodes and connect if node and adj node satisfy
-            }
-        } 
-        tc - n^2*n^2 = n^4
-        */
-        //dijkstra:
         int n = grid.size();
-        priority_queue <
-            pair<int, pair<int, int>>,
-            vector <pair<int, pair<int, int>>>,
-            greater <pair<int, pair<int, int>>>
-        > pq;
-        pq.push({grid[0][0], {0,0}});
-        vector <vector <int>> vis(n, vector <int> (n, 0));
-        vis[0][0] = 1;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int maxTillNow = it.first;
-            int r = it.second.first, c = it.second.second;
-            if(r == n-1 && c == n-1) return maxTillNow;
+        DSU obj(n*n);
+        map <int, pair<int,int>> coord;
+        for(int i = 0; i <n ;i++){
+            for(int j = 0; j< n; j++){
+                coord[grid[i][j]] = {i,j};
+            }
+        }
+        int startNode = 0, endNode = n*n-1;
+        for(auto it : coord){ //traversingin increasing order of heights -> increasing time
+            int height = it.first; // this height gets submerged now so connect it with neghbouring submeerged cells
+            int r = it.second.first;
+            int c = it.second.second;
+            int node = r*n + c;
+            // nhi hua to connect with 4 dir cells
             vector <int> move = {0,1,0,-1,0};
             for(int i = 0 ; i < 4 ; i++){
-                int nr = r+move[i], nc = c + move[i+1];
+                int nr = r + move[i], nc = c + move[i+1];
                 if(nr < 0 || nc < 0 || nr >= n || nc >= n) continue;
-                if(vis[nr][nc] == 1) continue;
-                vis[nr][nc] = 1;
-                pq.push({max(maxTillNow, grid[nr][nc]), {nr, nc}});
+                if(grid[nr][nc] > height) continue;
+                int adNode = nr*n + nc;
+                obj.unionByRank(node, adNode);
             }
+            if(obj.findParent(endNode) == obj.findParent(startNode)) return height;
         }
         return -1;
     }
