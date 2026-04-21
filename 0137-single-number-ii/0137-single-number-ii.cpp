f97fd302c5c7a7af340daa730e318a8624971900
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-04-21 20:49:15 +0530

    Time: 2 ms (38.56%), Space: 13.4 MB (35.07%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 1 ; i< n ; i += 3){
-            //check left right
-            if(nums[i-1] == nums[i+1]) continue;
-            if(nums[i-1] == nums[i]) return nums[i+1];
-            return nums[i-1];
+        // for(int i = 1 ; i< n ; i += 3){
+        //     //check left right
+        //     if(nums[i-1] == nums[i+1]) continue;
+        //     if(nums[i-1] == nums[i]) return nums[i+1];
+        //     return nums[i-1];
+        // }
+        // return nums[n-1];
+        int ans = 0;
+        for(int i = 0; i <= 31 ; i++){
+            int numSetBit = 0;
+            for(int j = 0 ; j < n ; j++){
+                if(nums[j]&(1<<i)) numSetBit++;
+            }
+            if((numSetBit%3) != 0) ans += (1<<i);
         }
-        return nums[n-1];
+        return ans;
     }
 };
