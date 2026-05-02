f56eb91094d5fcb3d4dc1e8cb18e170c46c7b5b3
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-05-02 19:39:19 +0530

    Time: 96 ms (22.51%), Space: 48.1 MB (17.95%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector <int> dist(n+1, INT_MAX);
-        dist[k] = 0;
-        vector <vector<pair<int, int>>> adj(n+1, vector <pair<int,int>> ());
-        for(auto time : times){
-            int u = time[0], v = time[1], w = time[2];
+        vector <int> dist(n, INT_MAX);
+        vector <vector <pair<int, int>>> adj(n, vector <pair<int, int>> ());
+        for(auto it : times){
+            int u = it[0], v = it[1], w = it[2];
+            u--, v--;
             adj[u].push_back({v, w});
         }
-        priority_queue <
-            pair <int, int>,
-            vector <pair<int,int>>,
-            greater <pair<int, int>>
-        > pq;
-        pq.push({0, k});
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int node = it.second, d = it.first;
-            if(dist[node] < d) continue;
-            for(auto adjNode : adj[node]){
-                int adjN = adjNode.first, time = adjNode.second;
-                int newTime = d + time;
-                if(dist[adjN] > newTime){
-                    dist[adjN] = newTime;
-                    pq.push({newTime, adjN});
+        dist[k-1] = 0;
+        priority_queue <pair<int, int>, vector <pair<int, int>>, greater <pair<int, int>>> q;
+        q.push({0, k-1});
+        while(!q.empty()){
+            int node = q.top().second;
+            int d = q.top().first;
+            q.pop();
+            if(dist[node] < d) continue; // <<-- optimisationm
+            for(auto ad : adj[node]){
+                int adNode = ad.first, wt = ad.second;
+                if(d + wt < dist[adNode]){
+                    dist[adNode] = d+wt;    
+                    q.push({dist[adNode], adNode});
                 }
             }
         }
         int ans = INT_MIN;
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 0 ; i < n ; i++){
             int it = dist[i];
             if(it == INT_MAX) return -1;
             ans = max(ans, it);
