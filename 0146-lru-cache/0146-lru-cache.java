357a280e400bec01fc0652eb9e44a2f9c6f48930
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-05-09 16:46:20 +0530

    Time: 48 ms (43.86%), Space: 130.2 MB (95.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,9 +1,15 @@
-class Node{
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Node {
+
     int key;
     int val;
+
     Node prev;
     Node next;
-    Node(int key, int val){
+
+    Node(int key, int val) {
         this.key = key;
         this.val = val;
     }
@@ -11,67 +17,116 @@ class Node{
 
 class LRUCache {
 
-    int cap;
-    Node head;
-    Node tail;
-    HashMap <Integer, Node> mp;
+    private final int cap;
+
+    private final HashMap<Integer, Node> mp;
+
+    private final Node head;
+    private final Node tail;
+
+    // explicit lock
+    private final ReentrantLock lock = new ReentrantLock();
 
     public LRUCache(int capacity) {
+
         this.cap = capacity;
+
         mp = new HashMap<>();
-        head = new Node(-1,-1);
+
+        // dummy nodes
+        head = new Node(-1, -1);
         tail = new Node(-1, -1);
-        head.next= tail;
+
+        head.next = tail;
         tail.prev = head;
     }
-    
-    public void deleteNode(Node node){
+
+    // remove node from DLL
+    private void deleteNode(Node node) {
+
         Node pre = node.prev;
         Node nxt = node.next;
+
         pre.next = nxt;
         nxt.prev = pre;
     }
 
-    public void insertAfterHead(Node node){
-        Node tmp = head.next;
+    // insert right after head
+    private void insertAfterHead(Node node) {
+
+        Node temp = head.next;
+
         head.next = node;
         node.prev = head;
-        tmp.prev = node;
-        node.next = tmp;
+
+        node.next = temp;
+        temp.prev = node;
     }
 
     public int get(int key) {
-        if(!mp.containsKey(key)){
-            return -1;
-        }
-        Node node = mp.get(key);
-        deleteNode(node);
-        insertAfterHead(node);
-        return node.val;
-    }
-    
-    public void put(int key, int value) {
-        if(mp.containsKey(key)){
+
+        lock.lock();
+
+        try {
+
+            if(!mp.containsKey(key)) {
+                return -1;
+            }
+
             Node node = mp.get(key);
-            node.val = value;
+
+            // move to front
             deleteNode(node);
             insertAfterHead(node);
-        }else{
-            if(mp.size() == cap){
-                Node lru = tail.prev;
-                deleteNode(lru);
-                mp.remove(lru.key);
+
+            return node.val;
+
+        } finally {
+
+            // ALWAYS unlock
+            lock.unlock();
+        }
+    }
+
+    public void put(int key, int value) {
+
+        lock.lock();
+
+        try {
+
+            // key already exists
+            if(mp.containsKey(key)) {
+
+                Node node = mp.get(key);
+
+                node.val = value;
+
+                deleteNode(node);
+                insertAfterHead(node);
+
+            } else {
+
+                // cache full
+                if(mp.size() == cap) {
+
+                    Node lru = tail.prev;
+
+                    deleteNode(lru);
+
+                    mp.remove(lru.key);
+                }
+
+                Node newNode = new Node(key, value);
+
+                insertAfterHead(newNode);
+
+                mp.put(key, newNode);
             }
-            Node node = new Node(key, value);
-            insertAfterHead(node);
-            mp.put(key, node);
+
+        } finally {
+
+            // ALWAYS unlock
+            lock.unlock();
         }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
