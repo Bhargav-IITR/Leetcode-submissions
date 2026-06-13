8fb363ffe49d1c037e8fdc4cf32cd6437b6219e2
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-06-13 22:18:52 +0530

    Time: 2968 ms (5.01%), Space: 583.6 MB (5.55%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,49 +1,72 @@
 class Twitter {
 public:
-    map <int, vector <pair<int, int>>> mp; // userId - vector of tweetIds of that userId
-    vector <vector <int>> flw;
+    map<int, vector<pair<int, int>>> mp; // userId -> {timestamp, tweetId}
+    map<int, set<int>> flw;              // follower -> followees
+    map<int, vector<int>> feed;
     int timer;
-    Twitter(){
-        this->timer = 0;
-        this->flw  = vector(501, vector <int> (501, 0));
-        for(int i = 0; i < 501 ; i++) flw[i][i] = 1;
+
+    Twitter() {
+        timer = 0;
     }
 
-    vector <int> refreshFeed(int userId){
-        vector <int> followeeIds;
-        for(int i = 0 ; i < 501 ; i++) if(flw[userId][i]) followeeIds.push_back(i);
-        priority_queue <pair<int, int>> pq;
-        for(auto followeeId : followeeIds){
-            for(int i = mp[followeeId].size()-1 ; i >= 0 ; i--){
-                auto tweet = mp[followeeId][i];
+    void refreshFeed(int userId) {
+        priority_queue<pair<int, int>> pq;
+
+        // User's own tweets
+        for (auto tweet : mp[userId]) {
+            pq.push(tweet);
+        }
+
+        // Tweets from followees
+        for (auto followeeId : flw[userId]) {
+            for (auto tweet : mp[followeeId]) {
                 pq.push(tweet);
             }
         }
-        vector <int> finalFeed;
-        int size = 10;
-        while(!pq.empty() && (size > 0)){
+
+        vector<int> finalFeed;
+        int sz = 10;
+
+        while (!pq.empty() && sz--) {
             finalFeed.push_back(pq.top().second);
             pq.pop();
-            size--;
         }
-        return finalFeed;
+
+        feed[userId] = finalFeed;
     }
-    
+
     void postTweet(int userId, int tweetId) {
         timer++;
         mp[userId].push_back({timer, tweetId});
+
+        // Refresh own feed
+        refreshFeed(userId);
+
+        // Refresh feeds of followers who follow this user
+        for (auto &[follower, followees] : flw) {
+            if (followees.count(userId)) {
+                refreshFeed(follower);
+            }
+        }
     }
-    
+
     vector<int> getNewsFeed(int userId) {
-        return refreshFeed(userId);
+        refreshFeed(userId);   // ensure latest feed
+        return feed[userId];
     }
-    
+
     void follow(int followerId, int followeeId) {
-        flw[followerId][followeeId] = 1;
+        if (followerId == followeeId) return;
+
+        flw[followerId].insert(followeeId);
+        refreshFeed(followerId);
     }
-    
+
     void unfollow(int followerId, int followeeId) {
-        flw[followerId][followeeId] = 0;
+        if (followerId == followeeId) return;
+
+        flw[followerId].erase(followeeId);
+        refreshFeed(followerId);
     }
 };
 
