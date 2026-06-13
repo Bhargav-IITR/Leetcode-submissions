83bf35592d389c4197de3f8350b8379ecdf9d8f7
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-06-13 22:20:09 +0530

    Time: 12 ms (32.44%), Space: 55.7 MB (94.62%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,80 +1,48 @@
 class Twitter {
 public:
-    map<int, vector<pair<int, int>>> mp; // userId -> {timestamp, tweetId}
-    map<int, set<int>> flw;              // follower -> followees
-    map<int, vector<int>> feed;
+    map<int, vector<pair<int, int>>> tweets; // userId -> {timestamp, tweetId}
+    map<int, set<int>> follows;
     int timer;
 
     Twitter() {
         timer = 0;
     }
 
-    void refreshFeed(int userId) {
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({++timer, tweetId});
+    }
+
+    vector<int> getNewsFeed(int userId) {
         priority_queue<pair<int, int>> pq;
 
-        // User's own tweets
-        for (auto tweet : mp[userId]) {
+        // Own tweets
+        for (auto &tweet : tweets[userId]) {
             pq.push(tweet);
         }
 
-        // Tweets from followees
-        for (auto followeeId : flw[userId]) {
-            for (auto tweet : mp[followeeId]) {
+        // Followees' tweets
+        for (int followee : follows[userId]) {
+            for (auto &tweet : tweets[followee]) {
                 pq.push(tweet);
             }
         }
 
-        vector<int> finalFeed;
-        int sz = 10;
-
-        while (!pq.empty() && sz--) {
-            finalFeed.push_back(pq.top().second);
+        vector<int> ans;
+        while (!pq.empty() && ans.size() < 10) {
+            ans.push_back(pq.top().second);
             pq.pop();
         }
 
-        feed[userId] = finalFeed;
-    }
-
-    void postTweet(int userId, int tweetId) {
-        timer++;
-        mp[userId].push_back({timer, tweetId});
-
-        // Refresh own feed
-        refreshFeed(userId);
-
-        // Refresh feeds of followers who follow this user
-        for (auto &[follower, followees] : flw) {
-            if (followees.count(userId)) {
-                refreshFeed(follower);
-            }
-        }
-    }
-
-    vector<int> getNewsFeed(int userId) {
-        refreshFeed(userId);   // ensure latest feed
-        return feed[userId];
+        return ans;
     }
 
     void follow(int followerId, int followeeId) {
         if (followerId == followeeId) return;
-
-        flw[followerId].insert(followeeId);
-        refreshFeed(followerId);
+        follows[followerId].insert(followeeId);
     }
 
     void unfollow(int followerId, int followeeId) {
         if (followerId == followeeId) return;
-
-        flw[followerId].erase(followeeId);
-        refreshFeed(followerId);
+        follows[followerId].erase(followeeId);
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
