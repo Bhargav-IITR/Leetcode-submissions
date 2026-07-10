26bec23e657b6fb5051d9f2340cfd2c260913a7e
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-07-10 10:55:19 +0530

    Time: 83 ms (14.92%), Space: 235.8 MB (27.35%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -2,24 +2,46 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         int i = 0;
-        vector <pair <int, int>> vp;
-        int start = 0;
+        vector <int> v(n, -1);
+        int start = 0, comp = 0;
         while(i < n){
             int numi = nums[i];
             int numj = nums[i] + maxDiff;
             int j = distance(nums.begin(), upper_bound(nums.begin(), nums.end(), numj)) - 1;
             if(j == i){
-                vp.push_back({start, j});
+                // vp.push_back({start, j});
+                v[start] = comp;
+                v[j] = comp;
                 start = i+1;
                 i++;
+                comp++;
             }
             else i = j;
         }
-        vector <int> v(n, -1);
-        for(int j = 0 ; j < vp.size() ; j++){
-            auto p = vp[j];
-            int start = p.first , end = p.second;
-            for(int k = start ; k <= end ; k++) v[k] = j;
+        
+        // for(int j = 0 ; j < vp.size() ; j++){
+        //     auto p = vp[j];
+        //     int start = p.first , end = p.second;
+        //     for(int k = start ; k <= end ; k++) v[k] = j;
+        // }
+        for (int i = 0; i < n; ) {
+            if (v[i] == -1) {
+                i++;
+                continue;
+            }
+
+            int j = i + 1;
+            while (j < n && v[j] == -1) {
+                j++;
+            }
+
+            if (j < n && v[j] == v[i]) {
+                for (int k = i + 1; k < j; k++) {
+                    v[k] = v[i];
+                }
+            }
+
+            i = j;
         }
         vector <bool> ans;
         for(auto query : queries){
