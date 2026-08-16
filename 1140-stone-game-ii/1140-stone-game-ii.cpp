a51c3e226559736a343e362fa510b85f9fe9de20
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-08-16 20:24:51 +0530

    Time: 11 ms (47.9%), Space: 11.3 MB (85.39%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
-    pair <int, int> dp[101][101][2][102];
+    pair<int, int> dp[101][2][102];
 
-    pair <int, int> func(int i, int j, int turn, int m, vector <int> &piles){
-        if(i > j) return {0,0};
-        if(dp[i][j][turn][m] != make_pair(-1,-1)) return dp[i][j][turn][m];
+    pair<int, int> func(int i, int turn, int m, vector<int>& piles) {
+        int n = piles.size();
+
+        if (i >= n) return {0, 0};
+
+        if (dp[i][turn][m] != make_pair(-1, -1))
+            return dp[i][turn][m];
+
         int sum = 0;
-        int X = 2*m;
+        int X = 2 * m;
+
+        pair<int, int> ans = {-1, -1};
         int maxi = INT_MIN;
-        pair <int, int> ans = {-1, -1};
-        for(int k = i ; k <= min(i+X-1, j) ; k++){
+
+        for (int k = i; k < min(i + X, n); k++) {
             sum += piles[k];
-            auto p = func(k+1, j, turn^1, max(m, k-i+1), piles);
-            if(turn){ // alice
-                if(sum + p.first > maxi){
-                    ans = make_pair(sum + p.first, p.second);
-                    maxi = sum + p.first;
-                }
-            }else{
-                if(sum + p.second > maxi){
-                    ans = make_pair(p.first, sum + p.second);
-                    maxi = sum + p.second;
+
+            auto p = func(
+                k + 1,
+                turn ^ 1,
+                max(m, k - i + 1),
+                piles
+            );
+
+            if (turn) { // Alice
+                int aliceScore = sum + p.first;
+
+                if (aliceScore > maxi) {
+                    maxi = aliceScore;
+                    ans = {aliceScore, p.second};
                 }
             }
-        } 
-        return dp[i][j][turn][m] = ans;
+            else { // Bob
+                int bobScore = sum + p.second;
+
+                if (bobScore > maxi) {
+                    maxi = bobScore;
+                    ans = {p.first, bobScore};
+                }
+            }
+        }
+
+        return dp[i][turn][m] = ans;
     }
 
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
         memset(dp, -1, sizeof(dp));
-        pair <int, int> ans = func(0, n-1, 1, 1, piles);
+
+        auto ans = func(0, 1, 1, piles);
+
         return ans.first;
     }
 };
