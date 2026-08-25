09e90a791862b9f65314c5d48e255556664884b7
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-08-26 00:06:11 +0530

    Time: 0 ms (100%), Space: 150.5 MB (72.11%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         int n = triplets.size();
-        map <int,int> mp;
-        for(int i = 0 ; i < n; i++){
-            if(triplets[i][0] <= target[0]) mp[i]++;
-            if(triplets[i][1] <= target[1]) mp[i]++;
-            if(triplets[i][2] <= target[2]) mp[i]++;
-        }
         vector <int> comp = {INT_MIN, INT_MIN, INT_MIN};
-        for(auto it : mp){
-            if(it.second == 3){
-                int id = it.first;
-                comp = {max(comp[0], triplets[id][0]), max(comp[1], triplets[id][1]), max(comp[2], triplets[id][2])};
+        for (auto &t : triplets) {
+            if (t[0] <= target[0] &&
+                t[1] <= target[1] &&
+                t[2] <= target[2]) {
+
+                comp[0] = max(comp[0], t[0]);
+                comp[1] = max(comp[1], t[1]);
+                comp[2] = max(comp[2], t[2]);
             }
         }
         return comp == target;
