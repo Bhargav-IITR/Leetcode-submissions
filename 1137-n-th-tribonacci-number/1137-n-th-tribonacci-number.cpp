46202890e6dfaa9c7f9efd0037aee057516c42e0
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-09-06 21:03:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.78%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t0 = 0, t1 = 1, t2 = 1;
+        if(n == 0) return t0;
+        if(n == 1) return t1;
+        if(n == 2 ) return t2;
+        int tn;
+        while(n-2){
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+            n--;
+        }
+        return tn;
+    }
+};
