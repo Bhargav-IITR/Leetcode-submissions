5d7842e394d72cd045871344a810539e0d810707
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-09-09 22:39:36 +0530

    Time: 21 ms (98.7%), Space: 25.8 MB (95.46%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,36 +1,44 @@
 #define ll long long
 class Solution {
 public:
-
+    int getNum(string &s){
+        int ans = 0;
+        int pow = 1;
+        for(int i = 3 ; i >= 0 ; i--){
+            ans +=( (s[i]-'0')*pow);
+            pow *= 10;
+        }
+        return ans;
+    }
     int openLock(vector<string>& deadends, string target) {
-        map <string, int> mp;
-        for(string s : deadends) mp[s]++;
+        vector<int> mp(10000,0);
+        for(string s : deadends) mp[getNum(s)] = 1;
         string curr = "0000";
         if(curr == target) return 0;
         queue <string> q;
         q.push(curr);
         int ans = 0;
-        map <string, int> vis;
-        vis[curr] = 1;
+        vector<int> vis(10000, 0);
+        vis[getNum(curr)] = 1;
         while(!q.empty()){
             int size = q.size();
             while(size--){
                 string front = q.front();
                 q.pop();
-                if(mp.find(front) != mp.end()) continue;
+                if(mp[getNum(front)] != 0) continue;
                 if(front == target) return ans;
                 for(int i = 0; i < 4 ; i++){ 
                     char c = front[i]; 
                     // up 
                     front[i] = (c == '9') ? '0' : (c+1); 
-                    if((mp.find(front) == mp.end()) && (vis.find(front) == vis.end())) { 
-                        vis[front] = 1;
+                    if((mp[getNum(front)] == 0) && (vis[getNum(front)] == 0)) { 
+                        vis[getNum(front)] = 1;
                         q.push(front);
                     } 
                     // down 
                     front[i] = (c == '0') ? '9' : (c-1); 
-                    if((mp.find(front) == mp.end()) && (vis.find(front) == vis.end())) { 
-                        vis[front] = 1;
+                    if((mp[getNum(front)] == 0) && (vis[getNum(front)] == 0)) { 
+                        vis[getNum(front)] = 1;
                         q.push(front);
                     } 
                     front[i] = c;
