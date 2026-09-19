6206e818d97f09a62003dbba96420c2054d86102
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2026-09-19 23:45:47 +0530

    Time: 38 ms (73.94%), Space: 38.6 MB (35.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,18 +14,30 @@ public:
         // memset(dp, -1, sizeof(dp));
         // return func(nums, nums.size()-1, nums.size());
         int n = nums.size();
-        vector <int> pre(n+1,0);
-        for(int j = 0 ; j <= n ; j++) pre[j] = 0;
-        for(int i = 1 ; i <= n ; i++){
-            vector <int> curr(n+1, 0);
-            for(int prev = i ; prev <= n ; prev++){
-                int notTake = pre[prev];
-                int take =0 ;
-                if(prev == n || nums[prev] > nums[i-1]) take = 1 + pre[i-1];
-                curr[prev] = max(take, notTake);
+        // vector <vector <int>> dp(n+1, vector <int> (n+1, -1));
+        // for(int j = 0 ; j <= n ; j++) dp[0][j] = 0;
+        // for(int i = 1 ; i <= n ; i++){
+        //     for(int prev = i ; prev <= n ; prev++){
+        //         int notTake = dp[i-1][prev];
+        //         int take =0 ;
+        //         if(prev == n || nums[prev] > nums[i-1]) take = 1 + dp[i-1][i-1];
+        //         dp[i][prev] = max(take, notTake);
+        //     }
+        // }
+        // return dp[n][n];
+        // most optimal solution -
+        vector <int> tails;
+        int ans = INT_MIN;
+        for(int i = 0 ; i < n ; i++){
+            int tailSize = tails.size();
+            int id = distance(tails.begin(), lower_bound(tails.begin(), tails.end(), nums[i]));
+            if(id == tailSize){
+                tails.push_back(nums[i]);
+                ans = max(ans, tailSize+1);
+            }else{
+                tails[id] = nums[i];
             }
-            pre = curr;
         }
-        return pre[n];
+        return ans;
     }
 };
